714926bd170bfb31a0ecc67f45421eb3658c08f6
Humayra <95118185+Humayra7@users.noreply.github.com>
2024-02-01 11:29:14 +0600

    classesObject.js

--- lab task1/classes&object.js
+++ lab task1/classes&object.js
@@ -22,3 +22,12 @@ class Student extends Person{
         console.log(`${this.name} 's grade is ${this.grade}.`)
     }
 }
+
+const alice =new Student('Alice',22,95);
+alice.speak();
+alice.study();
+
+console.log(Object.keys(alice));
+console.log(Object.values(alice));
+console.log(Object.entries(alice));
+
